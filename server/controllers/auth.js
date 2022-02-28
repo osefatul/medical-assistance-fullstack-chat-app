0e4b6765ca8d91f0e6005680a2a37835ab670ad1
osefatul <sefaullahomar2@gmail.com>
2022-02-27 20:37:43 -0800

    signup and login are done in auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,23 +4,13 @@ const StreamChat = require("stream-chat").StreamChat;
 const crypto = require("crypto");
 
 require("dotenv").config();
-
 const api_key = process.env.STREAM_API_KEY;
 const api_secret = process.env.STREAM_API_SECRET;
 const app_id = process.env.STREAM_APP_ID;
 
-const login = (req, res) => {
+const signup = async () => {
   try {
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: err });
-  }
-};
-
-const signup = () => {
-  try {
-    const { fullName, username, password, phoneNumber } = req.body;
-
+    const { fullName, username, password, phoneNumber } = re;
     const userId = crypto.randomBytes(16).toString("hex");
     const serverClient = connect(api_key, api_secret, app_id);
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -32,7 +22,42 @@ const signup = () => {
       username,
       userId,
       hashedPassword,
+      phoneNumber,
     });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const serverClient = connect(api_key, api_secret, app_id);
+    const client = StreamChat.getInstance(api_key, api_secret);
+
+    //find username matches in the users
+    const { users } = await client.queryUsers({ name: username });
+
+    if (!users.length)
+      return res.status(400).json({ message: "User not found" });
+
+    // if user is found then compare the password entered by user in the login page and in the client user
+    const success = await bcrypt.compare(password, users[0].hashedPassword);
+    const token = serverClient.createUserToken(users[0].id);
+
+    if (success) {
+      res
+        .status(200)
+        .json({
+          token,
+          fullName: users[0].fullName,
+          username,
+          userId: users[0].id,
+        });
+    } else {
+      res.status(500).json({ message: "Incorrect password" });
+    }
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: err });
