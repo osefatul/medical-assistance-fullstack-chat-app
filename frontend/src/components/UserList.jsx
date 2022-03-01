4988f70292f3d2983144edcb479d22c52c56d71b
osefatul <sefaullahomar2@gmail.com>
2022-02-28 23:09:03 -0800

    createChannel is done

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Avatar, useChatChannel } from "stream-chat-react";
-import { InviteICon } from "../assets";
+import { Avatar, useChatContext } from "stream-chat-react";
+import { InviteIcon } from "../assets";
 
-// All reacts component has access to specific props called "children". so whatever compnents you render inside of this. that is going to be populated in to the children prop. in our case it is UseList component and it will b populated in children
+// All react component has access to specific props called "children". so whatever compnents you render inside of this. that is going to be populated in to the children prop. in our case it is UseList component and it will b populated in children
 const ListContainer = ({ children }) => {
   return (
     <div className="user-list__container">
@@ -15,18 +15,103 @@ const ListContainer = ({ children }) => {
   );
 };
 
-const UserItem = () => {
+const UserItem = ({ user, setSelectedUsers }) => {
+  const [selected, setSelected] = useState(false);
+
+  const handleSelect = () => {
+    if (selected) {
+      setSelectedUsers((prevUsers) =>
+        prevUsers.filter((prevUser) => prevUser !== user.id)
+      );
+    } else {
+      setSelectedUsers((prevUsers) => [...prevUsers, user.id]);
+    }
+
+    setSelected((prevSelected) => !prevSelected);
+  };
   return (
-    <div className="user-item__wrapp">
+    <div className="user-item__wrapp" onClick={handleSelect}>
       <div className="user-item__name-wrapper">
-        <Avatar />
+        <Avatar image={user.image} name={user.fullName || user.id} size={32} />
+        <p className="user-item__name">{user.fullName || user.id}</p>
       </div>
+      {selected ? (
+        <InviteIcon />
+      ) : (
+        <div className="user-item__invite-empty"></div>
+      )}
     </div>
   );
 };
 
-const UserList = () => {
-  return <ListContainer className="">UserList</ListContainer>;
+const UserList = ({ setSelectedUsers }) => {
+  const { client } = useChatContext();
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [listEmpty, setListEmpty] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      if (loading) return;
+
+      setLoading(true);
+
+      try {
+        const response = await client.queryUsers(
+          { id: { $ne: client.userID } }, //remove the client userid
+          { id: 1 },
+          { limit: 8 }
+        );
+
+        if (response.users.length) {
+          setUsers(response.users);
+        } else {
+          setListEmpty(true);
+        }
+      } catch (error) {
+        setError(true);
+      }
+      setLoading(false);
+    };
+
+    if (client) getUsers();
+  }, []);
+
+  if (error) {
+    return (
+      <ListContainer>
+        <div className="user-list__message">
+          Error loading, please refresh and try again.
+        </div>
+      </ListContainer>
+    );
+  }
+
+  if (listEmpty) {
+    return (
+      <ListContainer>
+        <div className="user-list__message">No users found.</div>
+      </ListContainer>
+    );
+  }
+
+  return (
+    <ListContainer>
+      {loading ? (
+        <div className="user-list__message"> Loading users... </div>
+      ) : (
+        users?.map((user, i) => (
+          <UserItem
+            index={i}
+            key={user.i}
+            user={user}
+            setSelectedUsers={setSelectedUsers}
+          />
+        ))
+      )}
+    </ListContainer>
+  );
 };
 
 export default UserList;
