8f47e0492b30c1a0a4947022558b3024993759b5
osefatul <sefaullahomar2@gmail.com>
2022-03-01 23:57:03 -0800

    createChannel and userlist are done

--- frontend/src/components/EditChannel.jsx
+++ frontend/src/components/EditChannel.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useChatContext } from "stream-chat-react";
+import { UserList } from "./";
+import { CloseCreateChannel } from "../assets";
+
+const ChannelNameInput = ({ channelName = "", setChannelName }) => {
+  const handleChange = (event) => {
+    event.preventDefault();
+
+    setChannelName(event.target.value);
+  };
+
+  return (
+    <div className="channel-name-input__wrapper">
+      <p>Name</p>
+      <input
+        value={channelName}
+        onChange={handleChange}
+        placeholder="channel-name"
+      />
+      <p>Add Members</p>
+    </div>
+  );
+};
 
 function EditChannel() {
-  return (
-    <div>EditChannel</div>
-  )
+  const { channel } = useChatContext();
+  const [channelName, setChannelName] = useState(channel?.data?.name);
+  const [selectedUsers, setSelectedUsers] = useState([]);
+
+  return <div>EditChannel</div>;
 }
 
-export default EditChannel
+export default EditChannel;
