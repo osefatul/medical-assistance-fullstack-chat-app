de1d7fdee40eb8501240c02038332206577184d1
osefatul <sefaullahomar2@gmail.com>
2022-03-02 23:12:00 -0800

    channel search is updated

--- frontend/src/components/ChannelSearch.jsx
+++ frontend/src/components/ChannelSearch.jsx
@@ -3,6 +3,7 @@ import { useChatContext } from "stream-chat-react";
 
 import { SearchIcon } from "../assets";
 function ChannelSearch() {
+  const { client, setActiveChannel } = useChatContext();
   const [query, setQuery] = useState("");
   const [loading, setLoading] = useState(false);
 
